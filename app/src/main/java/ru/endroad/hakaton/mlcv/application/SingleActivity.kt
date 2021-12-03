9b5a957b056e1f3e8863cbcf52e5b9428117870a
Oleg Dvolyatik <oleg.dvolyatik@aviasales.ru>
2021-12-03 11:00:00 +0700

    Create activity

---
+++ app/src/main/java/ru/endroad/hakaton/mlcv/application/SingleActivity.kt
@@ -0,0 +1,8 @@
+package ru.endroad.hakaton.mlcv.application
+
+import androidx.appcompat.app.AppCompatActivity
+import ru.endroad.hakaton.mlcv.R
+
+class SingleActivity : AppCompatActivity(R.layout.base_activity) {
+
+}
