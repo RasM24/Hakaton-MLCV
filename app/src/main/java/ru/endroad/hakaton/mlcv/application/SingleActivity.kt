8f3c0115b1118a6f06096d63b23d3a0c83c1683e
Oleg Dvolyatik <oleg.dvolyatik@aviasales.ru>
2021-12-03 16:20:26 +0700

    Apply CameraX

--- app/src/main/java/ru/endroad/hakaton/mlcv/application/SingleActivity.kt
+++ app/src/main/java/ru/endroad/hakaton/mlcv/application/SingleActivity.kt
@@ -1,48 +1,75 @@
 package ru.endroad.hakaton.mlcv.application
 
 import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
 import ru.endroad.hakaton.mlcv.R
+import ru.endroad.hakaton.mlcv.permissions.haveCameraPermission
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class SingleActivity : AppCompatActivity(R.layout.base_activity) {
 
+	private val viewFinder: PreviewView
+		get() = findViewById(R.id.viewFinder)
+
 	private lateinit var cameraExecutor: ExecutorService
+
+	private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+		if (isGranted) {
+			startCamera()
+		} else
+			Unit
+	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-
-		// Request camera permissions
-		if (allPermissionsGranted()) {
-			startCamera()
-		} else {
-			ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+		when {
+			haveCameraPermission -> Unit
+			else                 -> requestPermissionLauncher.launch(Manifest.permission.CAMERA)
 		}
-
 
 		cameraExecutor = Executors.newSingleThreadExecutor()
 	}
 
-	private fun startCamera() {}
+	private fun startCamera() {
+		val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
-	private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-		ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+		cameraProviderFuture.addListener({
+			// Used to bind the lifecycle of cameras to the lifecycle owner
+			val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+			// Preview
+			val preview = Preview.Builder()
+				.build()
+				.also { it.setSurfaceProvider(viewFinder.surfaceProvider) }
+
+			// Select back camera as a default
+			val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+			try {
+				// Unbind use cases before rebinding
+				cameraProvider.unbindAll()
+
+				// Bind use cases to camera
+				cameraProvider.bindToLifecycle(
+					this, cameraSelector, preview
+				)
+
+			} catch (exc: Exception) {
+			}
+
+		}, ContextCompat.getMainExecutor(this))
 	}
 
 	override fun onDestroy() {
 		super.onDestroy()
 		cameraExecutor.shutdown()
-	}
-
-	companion object {
-
-		private const val TAG = "CameraXBasic"
-		private const val REQUEST_CODE_PERMISSIONS = 10
-		private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
 	}
 }
