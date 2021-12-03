9b68da53462303c1c8e1ff7449dc3a883ac8bcbc
Oleg Dvolyatik <oleg.dvolyatik@aviasales.ru>
2021-12-04 02:36:25 +0700

    Last Step

--- app/src/main/java/ru/endroad/hakaton/mlcv/processing/Processing.kt
+++ app/src/main/java/ru/endroad/hakaton/mlcv/processing/Processing.kt
@@ -4,19 +4,20 @@ import ru.endroad.hakaton.mlcv.application.AlertLuminosityStatus
 
 object Processing {
 
-	private val trainNumberRegex = Regex.fromLiteral("([0-9]{4})")
+	private val trainNumberRegex = Regex.fromLiteral("([0-9]{8})")
 	private val autoNumber = Regex.fromLiteral("/^[АВЕКМНОРСТУХ]\\d{3}(?<!000)[АВЕКМНОРСТУХ]{2}\\d{2,3}\$/ui")
 
 	fun processText(data: List<String>): String =
-		data.filter { it.matches(trainNumberRegex) || it.matches(autoNumber) }
+		data
+//			.filter { it.matches(trainNumberRegex) || it.matches(autoNumber) }
 			.joinToString(prefix = "Номера транспортных средств: ")
 
-	fun processLabels(data: List<String>): String = data[0]
+	fun processLabels(data: List<String>): String = data.joinToString()
 
 	fun processLuminosity(data: Double): AlertLuminosityStatus =
 		when {
-			data < 10.0 -> AlertLuminosityStatus.DARK
-			data > 120.0 -> AlertLuminosityStatus.LIGHT
-			else        -> AlertLuminosityStatus.NORMAL
+			data < 10.0  -> AlertLuminosityStatus.DARK
+			data > 140.0 -> AlertLuminosityStatus.LIGHT
+			else         -> AlertLuminosityStatus.NORMAL
 		}
 }
